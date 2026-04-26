98393a2d00334de67c5f21c2e0822562183a0951
DA COSTA Raphaël <137094345+Ropahel@users.noreply.github.com>
2026-04-26 17:46:29 +0200

    Add: top-k-elements-in-list - submission-6

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-6.py
@@ -0,0 +1,36 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        dico_nums = {}
+
+        for num in nums:
+            dico_nums[f"{num}"] = 1 + dico_nums.get(f"{num}", 0)
+
+        list_frequence = [[] for _ in range(len(nums))]
+
+        for str_nums in dico_nums:
+            if not list_frequence[dico_nums[str_nums] - 1 ]:
+                list_frequence[dico_nums[str_nums] - 1] = [int(str_nums)]
+            else:
+                list_frequence[dico_nums[str_nums] - 1].append(int(str_nums))
+
+        list_output = []
+        i = len(list_frequence)-1
+
+        while i  >= 0:
+            if len(list_output) == k:
+                return list_output
+            
+            if list_frequence[i]:
+                for nb in list_frequence[i]:
+                    list_output.append(nb)
+                    if len(list_output) == k:
+                        return list_output
+            i -=1
+
+
+
+
+
+
+
+
